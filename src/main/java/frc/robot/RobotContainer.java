dd177f5930738a4f9c603671deb32b65de3026dd
Ben Bell <110692534+slowpoke111@users.noreply.github.com>
2026-01-17 22:02:42 -0500

    added placeholder tunerconstants and autobuilder/swerve stuff(REgenerate tunerconstants.java before use)
    Regenerate TunerConstant.java before use since it is a placeholder which throws an exception at runtime

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -46,6 +46,8 @@ public class RobotContainer {
     new Trigger(m_exampleSubsystem::exampleCondition)
         .onTrue(new ExampleCommand(m_exampleSubsystem));
 
+//Regenerate tuner constants before doing anything with swerve
+
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());

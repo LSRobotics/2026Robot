68e1a19fbdd3e869dd6b6d5bb7936a3e90044dca
Marty Bonifanti <martinbonifanti2029@lschs.org>
2026-02-21 10:59:18 -0500

    Bug fixing in RunIndexerCommand

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026Robot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 42;
-  public static final String GIT_SHA = "e2d6deaf15bccebc7e24cdea6c5a4dabc3dd8b20";
-  public static final String GIT_DATE = "2026-02-15 13:31:10 EST";
+  public static final int GIT_REVISION = 45;
+  public static final String GIT_SHA = "2e713c124df65bf781788a0322906975da31956e";
+  public static final String GIT_DATE = "2026-02-21 09:35:07 EST";
   public static final String GIT_BRANCH = "spindexer";
-  public static final String BUILD_DATE = "2026-02-21 09:21:43 EST";
-  public static final long BUILD_UNIX_TIME = 1771683703842L;
+  public static final String BUILD_DATE = "2026-02-21 10:58:18 EST";
+  public static final long BUILD_UNIX_TIME = 1771689498681L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}

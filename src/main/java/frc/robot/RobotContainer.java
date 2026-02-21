f48dd7b190ccb8553ab37ce82ec2c12bcadb8aea
Ben Bell <110692534+slowpoke111@users.noreply.github.com>
2026-02-21 09:49:03 -0500

    Removed empty kicker files

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,7 +17,6 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.RunIntakeCommand;
 import frc.robot.subsystems.ExampleSubsystem;
-
 import frc.robot.util.SendableSupplier;
 import frc.robot.subsystems.arm.ArmConstants;
 
@@ -26,9 +25,7 @@ import static edu.wpi.first.units.Units.Degrees;
 import java.time.Instant;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
-
 import com.pathplanner.lib.auto.AutoBuilder;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;

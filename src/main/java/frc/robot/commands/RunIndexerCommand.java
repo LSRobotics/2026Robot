1201631c193e4072198d4792bc7b8c98ff6686a0
Marty Bonifanti <martinbonifanti2029@lschs.org>
2026-02-21 12:08:59 -0500

    Implemented the kicker into the RunIndexerCommand

--- src/main/java/frc/robot/commands/RunIndexerCommand.java
+++ src/main/java/frc/robot/commands/RunIndexerCommand.java
@@ -4,26 +4,30 @@ package frc.robot.commands;
 import frc.robot.subsystems.spindexer.SpindexerConstants;
 import frc.robot.subsystems.spindexer.SpindexerSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.kicker.KickerSubsystem;
+import frc.robot.subsystems.kicker.KickerConstants;
 
 
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.units.measure.AngularVelocity;
+import static edu.wpi.first.units.Units.RPM;
 
 public class RunIndexerCommand extends Command {
     @SuppressWarnings("PMD.UnusedPrivateField")
 
   private final SpindexerSubsystem spindexer;
 
-  //private final KickerSubsystem kicker;
+  private final KickerSubsystem kicker;
   
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public RunIndexerCommand(SpindexerSubsystem spindexer) {
+  public RunIndexerCommand(SpindexerSubsystem spindexer, KickerSubsystem kicker) {
     this.spindexer = spindexer;
-    //this.kicker = kicker;
+    this.kicker = kicker;
     
 
     // Use addRequirements() here to declare subsystem dependencies.
@@ -34,24 +38,24 @@ public class RunIndexerCommand extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    spindexer.runSpindexer(SpindexerConstants.SPINDEXER_SPEED);
-    //kicker.runKicker(KickerConstants.KICKER_SPEED);
+    //spindexer.runSpindexer(SpindexerConstants.SPINDEXER_SPEED);
+    kicker.runKicker(KickerConstants.KICKER_SPEED);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //kicker.runKicker(KickerConstants.KICKER_SPEED);
-    //if (kicker.getKickerSpeed() >= SpindexerConstants.SPINDEXER_MIN_SPEED) {
-    //    spindexer.runSpindexer(SpindexerConstants.SPINDEXER_SPEED);
-    //}
+    kicker.runKicker(KickerConstants.KICKER_SPEED);
+    if (kicker.getKickerSpeed().gte(AngularVelocity.ofBaseUnits(KickerConstants.KICKER_SPEED, RPM))) {
+       spindexer.runSpindexer(SpindexerConstants.SPINDEXER_SPEED);
+    }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     spindexer.runSpindexer(0);
-    //kicker.runKicker(0);
+    kicker.runKicker(0);
   }
 
   // Returns true when the command should end.

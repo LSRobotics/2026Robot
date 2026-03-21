89798d5ebbd089b0195fb724d2faebe831423f30
Ben Bell <110692534+slowpoke111@users.noreply.github.com>
2026-03-21 15:05:08 -0400

    Approved Auton Change

--- src/main/java/frc/robot/subsystems/kicker/KickerConstants.java
+++ src/main/java/frc/robot/subsystems/kicker/KickerConstants.java
@@ -1,13 +1,16 @@
 package frc.robot.subsystems.kicker;
 
+import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Degree;
 import static edu.wpi.first.units.Units.RPM;
 
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
 
 public class KickerConstants {
     public static final int KICKER_MOTOR_ID = 61; 
-    public static final double KICKER_SPEED = -1d; 
+    public static final double KICKER_SPEED = -0.8; 
     public static final double OUTTAKE_SPEED = -KICKER_SPEED;
+    public static final Current KickerCurrentLimit = Amps.of(40);
 }

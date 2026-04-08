e84bf75010145121d6ba30314e283f92a3572f37
Ben Bell <110692534+slowpoke111@users.noreply.github.com>
2026-04-08 11:57:47 -0400

    Log auton trajectory

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -16,6 +16,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.util.PathPlannerLogging;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -254,6 +255,10 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
             // Handle exception as needed
             e.printStackTrace();
         }
+
+        PathPlannerLogging.setLogActivePathCallback((poses) -> {
+            m_Field.getObject("path").setPoses(poses);
+        });
     }
 
     /**

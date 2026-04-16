78548058ca937573f9575ee92f01fe8286d87cba
GabrielKuzowsky <110692534+slowpoke111@users.noreply.github.com>
2026-04-16 17:31:33 -0400

    Shooting

--- src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
@@ -15,9 +15,9 @@ import edu.wpi.first.math.Matrix;
 
 public class VisionConstants {
     public static AprilTagFieldLayout aprilTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded);
-    public final static String camera0name = "Arducam_OV9281_USB_Camera (1)"; //Left
-    public final static String camera1name = "Arducam_OV9281_USB_Camera (2)"; //Right
-    public final static String camera2name = "Arducam_OV9281_USB_Camera"; //Back
+    public final static String camera0name = "Arducam_OV9281_USB_Camera (1)"; // Left
+    public final static String camera1name = "Arducam_OV9281_USB_Camera (2)"; // Right
+    public final static String camera2name = "Arducam_OV9281_USB_Camera";     // Back
 
     public final static Transform3d robotToCamera0 = new Transform3d(
             new Translation3d(Inches.of(2.325), Inches.of(14), Inches.of(19.5)),

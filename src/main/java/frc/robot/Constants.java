6b32d80f088e04396068908ff6756c7d88052613
Ben Bell <110692534+slowpoke111@users.noreply.github.com>
2026-05-24 09:45:28 -0400

    Slow Speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -52,8 +52,8 @@ public final class Constants {
     public static final int right6 = 15;
   }
 
-  public static double maxSpeedFast = 4.5;
-    public static double maxSpeedSlow = 4;
-    public static double MaxAngularSpeedNormal = RotationsPerSecond.of(0.75).in(RadiansPerSecond);
+  public static double maxSpeedFast = 2.5;
+    public static double maxSpeedSlow = 2d;
+    public static double MaxAngularSpeedNormal = RotationsPerSecond.of(0.55).in(RadiansPerSecond);
 
 }
